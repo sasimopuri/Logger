fc57a0a5116c56ae51b58eaa6902c1e6c79db70b
sasimopuri <sasikmopuriu@gmail.com>
2026-08-29 14:18:32 +0530

    Added DaisyUI, TailwindCSS

--- logger-ui/src/App.tsx
+++ logger-ui/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import "./App.css";
 function App() {
   return (
-   <>
-    <h1>Welcome to Logger</h1>
-   </>
-  )
+    <>
+      <h1>Welcome to Logger</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
